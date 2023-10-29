9485e830bda9cad74003d2e0a9be36fcad879b0b
Naim Caceres <naim.caceres1@gmail.com>
2023-10-29 13:11:38 -0300

    carrito

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ mostrarMenu()
 
 //Pre-entrega 2
 
-alert("Bienvenidos a El Rincon");
+/* alert("Bienvenidos a El Rincon");
 
 
 
@@ -116,7 +116,7 @@ const productos = [
     },
     {
         marca: `Puma`, genero: `calzado`, disciplina: `sportwear`, modelo: `Smash 3.0`, precio: 3990,
-    },
+    },  
     {
         marca: `Nike`, genero: `calzado`, disciplina: `futbol`, modelo: `Zoom Superfly 9 Academy`, precio: 5290,
     },
@@ -152,9 +152,8 @@ function agregarCarrito(producto) {
     alert(`${producto.marca} - ${producto.modelo} agregado al carrito.`);
 }
 
-
 function buscarProductos() {
-    const opcionBusqueda = prompt(`Elija la opcion deseada:\n1. Buscar por marca\n2. Buscar por genero\n3. Buscar por disciplina\n4. Buscar por modelo\n5. Volver`);
+    const opcionBusqueda = prompt(`Elija la opción deseada:\n1. Buscar por marca\n2. Buscar por genero\n3. Buscar por disciplina\n4. Buscar por modelo\n5. Volver`);
 
     if (opcionBusqueda !== "1" && opcionBusqueda !== "2" && opcionBusqueda !== "3" && opcionBusqueda !== "4" && opcionBusqueda !== "5") {
         alert("Opción incorrecta. Elija una opción válida.");
@@ -162,43 +161,62 @@ function buscarProductos() {
         return;
     }
 
+    if (opcionBusqueda === "5") {
+        verMenu();
+        return;
+    }
+
+    if (opcionBusqueda === "1") {
+        const productosEncontrados = buscarPorMarca();
+        mostrarResultados(productosEncontrados);
+        return;
+    }
+
+
+    const buscarPorMarca = () => {
+        let marcasDisponibles = "Marcas disponibles:\n";
+        productos.forEach((producto, index) => {
+            marcasDisponibles += `${index + 1}. ${producto.marca}\n`;
+        });
+        
+        marcasDisponibles += "Ingrese el número de la marca que desea buscar:";
+        
+        const marcaIndex = Number(prompt(marcasDisponibles));
+    
+        if (marcaIndex >= 1 && marcaIndex <= productos.length) {
+            const marcaElegida = productos[marcaIndex - 1].marca;
+            return productos.filter((producto) => producto.marca === marcaElegida);
+        } else {
+            alert("Opción no válida. Ingrese un número válido.");
+            return [];
+        }
+    }
+
     const palabraClave = prompt("Ingrese una palabra clave para buscar productos:");
-
     const valorBusqueda = palabraClave.toLowerCase();
-    
-    const buscarPorMarca = (producto) => producto.marca.toLowerCase().includes(valorBusqueda);
-    
-    const buscarPorGenero = (producto) => producto.genero.toLowerCase().includes(valorBusqueda);
-    
-    const buscarPorDisciplina = (producto) => producto.disciplina.toLowerCase().includes(valorBusqueda);
-    
-    const buscarPorModelo = (producto) => producto.modelo.toLowerCase().includes(valorBusqueda);
 
+    const buscarPorGenero = (producto) => producto.genero.toLowerCase().includes(valorBusqueda);
+    const buscarPorDisciplina = (producto) => producto.disciplina.toLowerCase().includes(valorBusqueda);
+    const buscarPorModelo = (producto) => producto.modelo.toLowerCase().includes(valorBusqueda);
 
     const productosEncontrados = productos.filter((producto) => {
         switch (opcionBusqueda) {
-            case "1":
-                return buscarPorMarca(producto);
             case "2":
                 return buscarPorGenero(producto);
             case "3":
                 return buscarPorDisciplina(producto);
             case "4":
                 return buscarPorModelo(producto);
-            case "5":
-                verMenu();
-                return;
             default:
                 alert("Opción incorrecta. Elija una opción válida.");
-                break
+                break;
         }
     });
 
-    if (opcionBusqueda === "5") {
-        verMenu();
-        return;
-    }
+    mostrarResultados(productosEncontrados);
+}
 
+function mostrarResultados(productosEncontrados) {
     if (productosEncontrados.length > 0) {
         let mensaje = "Productos encontrados:\n";
         productosEncontrados.forEach((producto, index) => {
@@ -221,6 +239,7 @@ function buscarProductos() {
         buscarProductos();
     }
 }
+
 
 
 function verCarrito() {
@@ -258,10 +277,167 @@ function verMenu() {
     } while (menu !== 3);
 
     alert("Muchas gracias por visitarnos, lo esperamos nuevamente.");
-}
+};
 
 
 verMenu();
+
+ */
+
+
+
+
+alert("Bienvenidos a El Rincon");
+
+let total = 0;
+let carrito = [];
+
+const productos = [
+    {
+        marca: `Nike`, genero: `calzado`, disciplina: `running`, modelo: `Flex Experience Run 11`, precio: 4690,
+    },
+    {
+        marca: `Nike`, genero: `calzado`, disciplina: `sportwear`, modelo: `Jordan Retro 3 White Cement (Reimagined)`, precio: 12990,
+    },
+    {
+        marca: `Adidas`, genero: `calzado`, disciplina: `sportwear`, modelo: `Court Plataform`, precio: 3690,
+    },
+    {
+        marca: `Puma`, genero: `calzado`, disciplina: `sportwear`, modelo: `Smash 3.0`, precio: 3990,
+    },
+    {
+        marca: `Nike`, genero: `calzado`, disciplina: `futbol`, modelo: `Zoom Superfly 9 Academy`, precio: 5290,
+    },
+    {
+        marca: `Adidas`, genero: `indumentaria`, disciplina: `sportwear`, modelo: `remera Brand Love`, precio: 1990,
+    },
+    {
+        marca: `Adidas`, genero: `indumentaria`, disciplina: `sportwear`, modelo: `Buzo Essential Brand Love`, precio: 5290,
+    },
+    {
+        marca: `Puma`, genero: `indumentraria`, disciplina: `training`, modelo: `Remera Uruguay Training`, precio: 2590,
+    },
+    {
+        marca: `Puma`, genero: `indumentaria`, disciplina: `training`, modelo: `W Calza Favorite Forever High Wasit`, precio: 2690,
+    },
+    {
+        marca: `Nike`, genero: `indumentaria`, disciplina: `sportwear`, modelo: `W Campera Club Hoodie Fz`, precio: 5290,
+    },
+    {
+        marca: `Puma`, genero: `accesorios`, disciplina: `futbol`, modelo: `Pelota Orbita La Liga`, precio: 1690,
+    },
+    {
+        marca: `Nike`, genero: `accesorios`, disciplina: `mochilas`, modelo: `Mochila Element`, precio: 1690,
+    },
+];
+
+function agregarCarrito(producto) {
+    carrito.push(producto);
+    total += producto.precio;
+    alert(`${producto.marca} - ${producto.modelo} agregado al carrito.`);
+}
+
+function buscarPorMarca() {
+    const marcas = [...new Set(productos.map(producto => producto.marca))];
+
+    let marcasDisp = "Marcas disponibles:\n";
+    marcasDisp += marcas.map((marca, index) => `${index + 1}. ${marca}`).join('\n');
+    marcasDisp += '\nElija el número de la marca que desea buscar:';
+    
+    const marcaBuscada = Number(prompt(marcasDisp));
+
+    if (marcaBuscada >= 1 && marcaBuscada <= marcas.length) {
+        const marcaElegida = marcas[marcaBuscada - 1];
+        const productosMarca = productos.filter(producto => producto.marca === marcaElegida);
+        mostrarResultados(productosMarca);
+    } else {
+        alert("Opción no válida. Ingrese un número válido.");
+        buscarProductos();
+    }
+}
+
+
+function buscarProductos() {
+    let opcionBusqueda;
+    do {
+        opcionBusqueda = prompt(`Elija la opción deseada:\n1. Buscar por marca\n2. Buscar por genero\n3. Buscar por disciplina\n4. Buscar por modelo\n5. Volver`);
+
+        switch (opcionBusqueda) {
+            case "1":
+                buscarPorMarca();
+                break;
+            case "5":
+                verMenu();
+                return;
+            default:
+                alert("Opción incorrecta. Elija una opción válida.");
+                break;
+        }
+    } while (opcionBusqueda !== "5");
+}
+
+
+function mostrarResultados(productosEncontrados) {
+    if (productosEncontrados.length > 0) {
+        let mensaje = "Productos encontrados:\n";
+        productosEncontrados.forEach((producto, index) => {
+            mensaje += `${index + 1}. ${producto.marca} ${producto.modelo} - Precio: ${producto.precio}\n`;
+        });
+        mensaje += `${productosEncontrados.length + 1}. Volver`;
+
+        const respuesta = prompt(mensaje);
+
+        if (respuesta <= productosEncontrados.length) {
+            agregarCarrito(productosEncontrados[respuesta - 1]);
+            buscarProductos();
+        } else if (respuesta == productosEncontrados.length + 1) {
+            buscarProductos();
+        } else {
+            alert("Elija una opción válida.");
+        }
+    } else {
+        alert("No se encontraron productos que coincidan con la búsqueda.");
+        buscarProductos();
+    }
+}
+
+function verCarrito() {
+    if (carrito.length !== 0) {
+        let carritoInfo = "Los productos agregados al carrito de compras son:\n";
+        carrito.forEach((producto) => {
+            carritoInfo += `${producto.marca} ${producto.modelo} - Precio: ${producto.precio}\n`;
+        });
+        carritoInfo += `Total: UYU ${total}`;
+        alert(carritoInfo);
+    } else {
+        alert("El carrito está vacío.");
+    }
+}
+
+function verMenu() {
+    let menu;
+    do {
+        menu = Number(prompt("Elija la opción que desea:\n1. Buscar productos\n2. Ver Carrito\n3. Salir"));
+        switch (menu) {
+            case 1:
+                buscarProductos();
+                break;
+            case 2:
+                verCarrito();
+                break;
+            case 3:
+                alert("Muchas gracias por visitarnos, lo esperamos nuevamente.");
+                break;
+            default:
+                alert("Opción no válida. Elija una opción válida.");
+                break;
+        }
+    } while (menu !== 3);
+    alert("Muchas gracias por visitarnos, lo esperamos nuevamente.");
+}
+
+verMenu();
+
 
 
 
